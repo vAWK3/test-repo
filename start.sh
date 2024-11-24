99659f67e408a0bdb14147a32ad8cd12764af88c
vic30004 <victor_993@msn.com>
2024-11-24 15:15:41 -0800

    start sh update

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Run database migrations
 export DJANGO_SETTINGS_MODULE=app.settings
-
-python manage.py makemigrations
-python manage.py migrate
 
 cd /app/app
 gunicorn app.wsgi:application --bind 0.0.0.0:${PORT:-8000}
